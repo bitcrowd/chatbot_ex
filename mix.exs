cf67203c99f666f76f9b47041000fd90f3c032e4
Hannah Voget <hannah@bitcrowd.net>
2024-10-24 15:25:25 +0200

    Add BitstylesPhoenix

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Chatbot.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:bitstyles_phoenix, "~> 2.5"}
     ]
   end
 
