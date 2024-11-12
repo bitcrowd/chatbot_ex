ec92259b82d9bdf2ac9f66c4e82c5a62d33b90ad
Hannah Voget <hannah@bitcrowd.net>
2024-11-12 14:56:04 +0100

    More message tests

--- test/chatbot/chat/message_test.exs
+++ test/chatbot/chat/message_test.exs
@@ -1,6 +1,21 @@
 defmodule Chatbot.Chat.MessageTest do
   use Chatbot.DataCase
+  import Chatbot.Factory
   alias Chatbot.Chat.Message
+
+  describe "table constraints" do
+    test "role is not nullable" do
+      assert_raise Postgrex.Error, ~r/null value in column "role"/, fn ->
+        insert(:message, role: nil)
+      end
+    end
+
+    test "content is not nullable" do
+      assert_raise Postgrex.Error, ~r/null value in column "content"/, fn ->
+        insert(:message, content: nil)
+      end
+    end
+  end
 
   describe "changeset/2" do
     test "is valid with valid params" do
@@ -13,6 +28,13 @@ defmodule Chatbot.Chat.MessageTest do
       %{"role" => "invalid role", "content" => "hello"}
       |> Message.changeset()
       |> assert_error_on(:role, "is invalid")
+    end
+
+    test "requires role and contents" do
+      %{}
+      |> Message.changeset()
+      |> assert_required_error_on(:role)
+      |> assert_required_error_on(:content)
     end
   end
 end
