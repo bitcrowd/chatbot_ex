d6ec16ebbb74ee65655ec00d2e6aa2386e446ec2
Hannah Voget <hannah@bitcrowd.net>
2024-11-13 18:15:00 +0100

    Update CI

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule Chatbot.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_file: {:no_warn, "priv/plts/chatbot.plt"}]
     ]
   end
 
