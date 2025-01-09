3999b7ea486c83efbaee095739a491d2e5ac9379
Joel Koch <joel@bitcrowd.net>
2025-01-09 20:45:04 +0100

    Restructure serving module

--- lib/chatbot/rag/serving.ex
+++ lib/chatbot/rag/serving.ex
@@ -1,10 +1,11 @@
 defmodule Chatbot.Rag.Serving do
+  @embedding_repo {:hf, "thenlper/gte-small" }
+  @llm_repo {:hf, "microsoft/phi-3.5-mini-instruct"}
+
   def build_embedding_serving() do
-    repo = {:hf, "thenlper/gte-small"}
+    {:ok, model_info} = Bumblebee.load_model(@embedding_repo)
 
-    {:ok, model_info} = Bumblebee.load_model(repo)
-
-    {:ok, tokenizer} = Bumblebee.load_tokenizer(repo)
+    {:ok, tokenizer} = Bumblebee.load_tokenizer(@embedding_repo)
 
     Bumblebee.Text.TextEmbedding.text_embedding(model_info, tokenizer,
       compile: [batch_size: 64, sequence_length: 512],
@@ -15,11 +16,9 @@ defmodule Chatbot.Rag.Serving do
   end
 
   def build_llm_serving() do
-    repo = {:hf, "microsoft/phi-4"}
-
-    {:ok, model_info} = Bumblebee.load_model(repo)
-    {:ok, tokenizer} = Bumblebee.load_tokenizer(repo)
-    {:ok, generation_config} = Bumblebee.load_generation_config(repo)
+    {:ok, model_info} = Bumblebee.load_model(@llm_repo)
+    {:ok, tokenizer} = Bumblebee.load_tokenizer(@llm_repo)
+    {:ok, generation_config} = Bumblebee.load_generation_config(@llm_repo)
 
     generation_config = Bumblebee.configure(generation_config, max_new_tokens: 1024)
 
@@ -28,5 +27,14 @@ defmodule Chatbot.Rag.Serving do
       defn_options: [compiler: EXLA],
       stream: true
     )
+  end
+
+  def load_all do
+    Bumblebee.load_model(@llm_repo)
+    Bumblebee.load_tokenizer(@llm_repo)
+    Bumblebee.load_generation_config(@llm_repo)
+
+    Bumblebee.load_model(@embedding_repo)
+    Bumblebee.load_tokenizer(@embedding_repo)
   end
 end
